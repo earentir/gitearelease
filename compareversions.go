b2c7da54c8b64e17aa16a60b248727c202331440
George Pantazis <george.pantazis@vodafoneinnovus.com>
2024-01-21 15:28:07 +0200

    ranamed vars

--- compareversions.go
+++ compareversions.go
@@ -6,41 +6,41 @@ import (
 	"strings"
 )
 
-// CompareVersions compares two version strings and returns 1 if v1 is greater than v2, -1 if v1 is less than v2, and 0 if v1 is equal to v2
-func CompareVersions(v1, v2 string) int {
+// CompareVersions compares two version strings and returns  -1 if own is older than current, 0 if own is equal to current and 1 if own is newer than current
+func CompareVersions(own, current string) int {
 	// Remove the version prefixs from the version strings
-	v1 = TrimVersionPrefix(v1)
-	v2 = TrimVersionPrefix(v2)
+	own = TrimVersionPrefix(own)
+	current = TrimVersionPrefix(current)
 
 	// Split the version strings into individual version numbers
-	v1Numbers := strings.Split(v1, ".")
-	v2Numbers := strings.Split(v2, ".")
+	ownNumbers := strings.Split(own, ".")
+	currentNumbers := strings.Split(current, ".")
 
 	// Convert the version numbers to integers and compare them
-	for i := 0; i < len(v1Numbers) && i < len(v2Numbers); i++ {
-		v1Num, err := strconv.Atoi(v1Numbers[i])
+	for i := 0; i < len(ownNumbers) && i < len(currentNumbers); i++ {
+		ownNum, err := strconv.Atoi(ownNumbers[i])
 		if err != nil {
-			fmt.Println("Invalid version number:", v1Numbers[i])
+			fmt.Println("Invalid version number:", ownNumbers[i])
 			return 0
 		}
-		v2Num, err := strconv.Atoi(v2Numbers[i])
+		currentNum, err := strconv.Atoi(currentNumbers[i])
 		if err != nil {
-			fmt.Println("Invalid version number:", v2Numbers[i])
+			fmt.Println("Invalid version number:", currentNumbers[i])
 			return 0
 		}
 
-		if v1Num > v2Num {
+		if ownNum > currentNum {
 			return 1
-		} else if v1Num < v2Num {
+		} else if ownNum < currentNum {
 			return -1
 		}
 	}
 
 	// If we got here, the version numbers are the same up to this point
 	// Check if one of the versions has more version numbers than the other
-	if len(v1Numbers) > len(v2Numbers) {
+	if len(ownNumbers) > len(currentNumbers) {
 		return 1
-	} else if len(v1Numbers) < len(v2Numbers) {
+	} else if len(ownNumbers) < len(currentNumbers) {
 		return -1
 	}
 
